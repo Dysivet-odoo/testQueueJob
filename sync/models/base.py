5886423b91636e7166d572320827b8050c5085da
Dysivet-odoo <100676315+Dysivet-odoo@users.noreply.github.com>
2023-09-11 21:59:22 +0300

    Twst2

---
+++ sync/models/base.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Ivan Yelizariev <https://twitter.com/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+
+from odoo import models
+
+
+class Base(models.AbstractModel):
+    _inherit = "base"
+
+    # delete_my_code_new
+    def set_link(self, relation_name, ref, bot_name, sync_date=None, allow_many2many=False):
+        return self.env["sync.link"]._set_link_odoo(
+            self, relation_name, ref, bot_name, sync_date, allow_many2many
+        )
+    # def set_link(self, relation_name, ref, sync_date=None, allow_many2many=False):
+    #     return self.env["sync.link"]._set_link_odoo(
+    #         self, relation_name, ref, sync_date, allow_many2many
+    #     )
+
+    # delete_my_code_new
+    def search_links(self, relation_name, bot_name, refs=None):
+        return (
+            self.env["sync.link"]
+            .with_context(sync_link_odoo_model=self._name)
+            ._search_links_odoo(self, relation_name, bot_name, refs)
+        )
+
+    # def search_links(self, relation_name, refs=None):
+    #     return (
+    #         self.env["sync.link"]
+    #         .with_context(sync_link_odoo_model=self._name)
+    #         ._search_links_odoo(self, relation_name, refs)
+    #     )
